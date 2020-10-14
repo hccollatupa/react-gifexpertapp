5b4a0f82fc9b6adcb9699c49ab8278ac9dcac1a8
Hector Ccollatupa <hccollatupa@gmail.com>
2020-10-14 12:26:53 -0500

    docs agregado

---
+++ docs/precache-manifest.21084c60b7d9425bb248fd74cce48186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfb16d97ac28414f034fe131e068b0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaaf5e4941add6ca8206",
+    "url": "/static/css/main.3d107ca5.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaaf5e4941add6ca8206",
+    "url": "/static/js/main.6156991b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
